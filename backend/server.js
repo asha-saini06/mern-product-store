a35044ad55dcfdca2032cb3879d19410baed2bca
Asha Saini <59530575+asha-saini06@users.noreply.github.com>
2024-08-10 23:47:54 +0530

    API to create a product

--- backend/server.js
+++ backend/server.js
@@ -6,9 +6,28 @@ dotenv.config();
 
 const app = express();
 
-app.get('/products', (req, res) => { });
+app.use(express.json()); // allows us to accept JSON data in the req.body
 
-// console.log(process.env.MONGO_URI);
+app.post('/api/products', async (req, res) => {
+    const product = req.body; // get the product from the request body
+
+    if (!product.name || !product.price || !product.image) {
+        return res.status(400).json({ success: false, message: 'All fields are required' });
+    }
+
+    const newProduct = new Product(product);
+    try {
+        await newProduct.save();
+        res.status(201).json({ success: true, data: newProduct });
+    }
+    catch {
+        console.error("Error in creating product: ", error.message);
+        res.status(500).json({ success: false, message: 'Internal server error' });
+    }
+
+});
+
+// To test this use Postman  
 
 app.listen(5000, () => {
     connectDB();
